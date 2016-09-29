9a14011eb44ce56ac11b1434b88cecc0e1f8df9e
Gonzalo <Gonzalo@Gonzalo>
2016-09-29 19:12:13 -0300

    [A]Cambios de cliente

--- ObligatorioAS_2016-war/src/java/servicios/service/ClienteResource.java
+++ ObligatorioAS_2016-war/src/java/servicios/service/ClienteResource.java
@@ -1,3 +1,71 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package servicios.service;
+//
+//import Entidades.ClienteEntity;
+//import com.google.gson.Gson;
+//import Dominio.ClienteBean;
+//import java.util.List;
+//import javax.ejb.EJB;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.Context;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import javax.ws.rs.core.UriInfo;
+//
+///**
+// *
+// * @author alvaro
+// */
+//@Path("cliente")
+//public class ClienteResource {
+//    
+//    @EJB
+//    private ClienteBean clienteBean;
+//    
+//    @Context
+//    private UriInfo context;
+//
+//    public ClienteResource() {
+//    }
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String getJson() {
+//        List<ClienteEntity> list = clienteBean.listar();
+//        Gson gson = new Gson();
+//        return gson.toJson(list);
+//    }
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response agregar(String body) {
+//        Gson gson = new Gson();
+//        ClienteEntity u = gson.fromJson(body, ClienteEntity.class);
+//        Response r;
+//        ClienteEntity creado = clienteBean.agregar(u);
+//        if (creado == null) {
+//            r = Response
+//                    .status(Response.Status.BAD_REQUEST)
+//                    .entity("afadfadf")
+//                    .build();
+//        } else {
+//            r = Response
+//                    .status(Response.Status.CREATED)
+//                    .entity(gson.toJson(creado))
+//                    .build();
+//        }
+//        return r;
+//    } 
+//    
+//}
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -37,6 +105,8 @@ public class ClienteResource {
     }
 
     @GET
+    
+    @Path("getJson")
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson() {
         List<ClienteEntity> list = clienteBean.listar();
@@ -45,6 +115,7 @@ public class ClienteResource {
     }
 
     @POST
+    @Path("agregarCliente")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response agregar(String body) {
         Gson gson = new Gson();
@@ -64,5 +135,46 @@ public class ClienteResource {
         }
         return r;
     } 
-    
+    @POST
+    @Path("modificarCliente")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response modificar(String body) {
+        Gson gson = new Gson();
+        ClienteEntity u = gson.fromJson(body, ClienteEntity.class);
+        Response r;
+        ClienteEntity modificado = clienteBean.modificar(u);
+        if (modificado == null) {
+            r = Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("afadfadf")
+                    .build();
+        } else {
+            r = Response
+                    .status(Response.Status.CREATED)
+                    .entity(gson.toJson(modificado))
+                    .build();
+        }
+        return r;
+    } 
+     @POST
+    @Path("eliminarCliente")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response eliminar(String body) {
+        Gson gson = new Gson();
+        ClienteEntity u = gson.fromJson(body, ClienteEntity.class);
+        Response r;
+        Boolean modificado = clienteBean.eliminar(u);
+        if (modificado == false) {
+            r = Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("afadfadf")
+                    .build();
+        } else {
+            r = Response
+                    .status(Response.Status.CREATED)
+                    .entity(gson.toJson(modificado))
+                    .build();
+        }
+        return r;
+    } 
 }
