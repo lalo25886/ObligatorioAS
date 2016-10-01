65894c829e4ace7447b058070f8bc6fc2d633077
artiguense <delarosa.alvarojose@gmail.com>
2016-10-01 11:44:50 -0300

    Update ClienteBean.java

--- ObligatorioAS_2016-ejb/src/java/dominio/ClienteBean.java
+++ ObligatorioAS_2016-ejb/src/java/dominio/ClienteBean.java
@@ -1,107 +1,29 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package Dominio;
-//
-//import Entidades.ClienteEntity;
-//import java.util.List;
-//import javax.annotation.PostConstruct;
-//import javax.ejb.Stateless;
-//import javax.ejb.LocalBean;
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//
-///**
-// *
-// * @author alvaro
-// */
-//@Stateless
-//@LocalBean
-//public class ClienteBean {
-//
-//   
-//    @PersistenceContext
-//    private EntityManager em;
-//    
-//    @PostConstruct
-//    private void init() {
-//        System.out.println("INSTANCIA CLIENTE BEAN");
-//    }
-//       
-//    public ClienteEntity agregar(ClienteEntity u) {
-//        return agregar(u.getNombre());
-//    }
-// 
-//    public ClienteEntity agregar(String nombre) {
-//        
-//        ClienteEntity u = new ClienteEntity();
-//        u.setNombre(nombre);
-//        
-//        
-//        em.persist(u);
-//        
-//        return u;
-//    }
-//    
-//    public ClienteEntity modificar(Long id, String nombreNuevo) {
-//        
-//        ClienteEntity u = em.find(ClienteEntity.class, id);
-//        
-//        u.setNombre(nombreNuevo);
-//        
-//        em.merge(u);
-//        
-//        return u;
-//    }
-//    
-//    public boolean eliminar(Long id) {
-//        ClienteEntity u = em.find(ClienteEntity.class, id);
-//        
-//        em.remove(u);
-//        
-//        return true;
-//    }
-//    
-//    public List<ClienteEntity> listar() {
-//        
-//        List<ClienteEntity> list = 
-//                em
-//                    .createQuery("select u from Cliente u")
-//                    .getResultList();
-//        
-//        return list;
-//    }
-//    
-//    public Cliente buscar(Long id) {
-//        ClienteEntity ent = em.find(ClienteEntity.class, id);
-//        Cliente u = new Cliente();
-//        u.setId(ent.getId());
-//        u.setNombre(ent.getNombre());
-//        return u;
-//    }
-//    
-//    public List<ClienteEntity> buscar(String nombre) {
-//        List<ClienteEntity> list = em.createQuery("select u from Cliente u "
-//        + "where u.nombre = :nombre").setParameter("nombre", nombre).getResultList();
-//        return list;
-//    }
-//
-//}
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dominio;
+package Dominio;
 
 import Entidades.ClienteEntity;
 import com.google.gson.Gson;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -112,18 +34,25 @@ import javax.persistence.PersistenceContext;
 @Stateless
 @LocalBean
 public class ClienteBean {
-
-   
+    private static final Logger LOGGER= Logger.getLogger("logalvaro.log");
+  
+    @Resource(lookup = "jms/ConnectionFactory")
+    private ConnectionFactory connectionFactory;
+    
+    @Resource(lookup = "jms/Queue")
+    private Queue queue;
+    
     @PersistenceContext
     private EntityManager em;
     
     @PostConstruct
     private void init() {
-        System.out.println("INSTANCIA CLIENTE BEAN");
+        //System.out.println("INSTANCIA CLIENTE BEAN");
     }
        
     public ClienteEntity agregar(ClienteEntity u) {
          em.persist(u);
+          enviarCreacionCliente(u);
          return u;
 
     }
@@ -133,7 +62,7 @@ public class ClienteBean {
        ClienteEntity u = gson.fromJson(body, ClienteEntity.class);
         
         em.persist(u);
-        
+        enviarCreacionCliente(u);
         return u;
     }
     
@@ -170,9 +99,29 @@ public class ClienteBean {
     }
     
     public List<ClienteEntity> buscar(String nombre) {
-        List<ClienteEntity> list = em.createQuery("select u from Cliente u "
+        List<ClienteEntity> list = em.createQuery("select u from ClienteEntity u "
         + "where u.nombre = :nombre").setParameter("nombre", nombre).getResultList();
         return list;
     }
+    private void enviarCreacionCliente(ClienteEntity unCliente) {
+        
+        try (Connection connection = connectionFactory.createConnection(); 
+            Session session = connection.createSession()) {
+            MessageProducer productorDeMensaje = session.createProducer(queue);
+            Message mensaje = session.createTextMessage("Cliente creado:" + unCliente.getNombre());
+            
+            productorDeMensaje.send(mensaje);
+            System.out.println("Cliente :" + unCliente.getNombre());
+            LOGGER.log(Level.FINEST,"Prueba logger");
+            
+        } catch (JMSException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public void metodo() throws IOException {
+    LogManager.getLogManager().readConfiguration(
+        new FileInputStream("./log.properties"));
+}
 
 }
