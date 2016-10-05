294b346efcc0e00a0ae3b57a934ef62985cb149f
artiguense <delarosa.alvarojose@gmail.com>
2016-10-04 22:54:10 -0300

    Mensajeria por 3 vias segun receptor emisor y cadete

--- ObligatorioAS_2016-ejb/src/java/ObligatorioAS_2016/cliente/ClienteBean.java
+++ ObligatorioAS_2016-ejb/src/java/ObligatorioAS_2016/cliente/ClienteBean.java
@@ -7,23 +7,10 @@ package ObligatorioAS_2016.cliente;
 
 import ObligatorioAS_2016.entidades.ClienteEntity;
 import com.google.gson.Gson;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -36,21 +23,7 @@ import javax.persistence.PersistenceContext;
 public class ClienteBean {
     
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger("FILE");
-    @Resource(lookup = "jms/ConnectionFactory")
-    private ConnectionFactory connectionFactory;
-    
-    @Resource(lookup = "jms/Queue")
-    private Queue queue;
-    
-    
-    @Resource(lookup = "jms/QueueCadete")
-    private Queue queueCadete;
-    
-    @Resource(lookup = "jms/QueueEmisor")
-    private Queue queueEmisor;
-    
-    @Resource(lookup = "jms/QueueReceptor")
-    private Queue queueReceptor;
+
     
     @PersistenceContext
     private EntityManager em;
@@ -61,42 +34,31 @@ public class ClienteBean {
     }
        
     public ClienteEntity agregar(ClienteEntity u) {
-         em.persist(u);
-          enviarCreacionCliente(u);
-         return u;
-
+        em.persist(u);
+        return u;
     }
  
     public ClienteEntity agregar(String body) {
        Gson gson = new Gson();
        ClienteEntity u = gson.fromJson(body, ClienteEntity.class);
-        
         em.persist(u);
-        enviarCreacionCliente(u);
         return u;
     }
     
     public ClienteEntity modificar(ClienteEntity u) {
-        
         em.merge(u);
-        
         return u;
     }
     
     public boolean eliminar(ClienteEntity u) {
        ClienteEntity aBorrar = em.find(ClienteEntity.class, u.getId());
         em.remove(aBorrar);
-        
         return true;
     }
     
     public List<ClienteEntity> listar() {
         
-        List<ClienteEntity> list = 
-                em
-                    .createQuery("select u from ClienteEntity u")
-                    .getResultList();
-        
+        List<ClienteEntity> list = em.createQuery("select u from ClienteEntity u").getResultList();
         return list;
     }
     
@@ -113,31 +75,4 @@ public class ClienteBean {
         + "where u.nombre = :nombre").setParameter("nombre", nombre).getResultList();
         return list;
     }
-    private void enviarCreacionCliente(ClienteEntity unCliente) {
-        
-        try (Connection connection = connectionFactory.createConnection(); 
-          Session session = connection.createSession()) {
-            MessageProducer productorDeMensajeCadete = session.createProducer(queueCadete);
-            MessageProducer productorDeMensajeEmisor = session.createProducer(queueEmisor);
-            MessageProducer productorDeMensajeReceptor = session.createProducer(queueReceptor);
-         
-            Message mensaje = session.createTextMessage("Estimado cadete tiene un envio pendiente:");
-            productorDeMensajeCadete.send(mensaje);
-            mensaje = session.createTextMessage("Estimado cliente su envio esta siendo creado:" );
-            productorDeMensajeEmisor.send(mensaje);
-            mensaje = session.createTextMessage("Querido cliente su envio esta siendo enviado :" );
-            productorDeMensajeReceptor.send(mensaje);
-            
-            log.info("Envio realizado");
-            
-        } catch (JMSException ex) {
-            log.error("ERROR:"  + ex.getMessage() );
-        }
-        
-    }
-    public void metodo() throws IOException {
-    LogManager.getLogManager().readConfiguration(
-        new FileInputStream("./log.properties"));
-}
-
 }
